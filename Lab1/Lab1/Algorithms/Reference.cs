e6e62a33a9f9dc34ff7d409c52592179a4db649e
Alina Annayeva <alina.annajeva@gmail.com>
2013-10-31 22:32:45 +0200

    Gauss method finished

--- Lab1/Lab1/Algorithms/Reference.cs
+++ Lab1/Lab1/Algorithms/Reference.cs
@@ -15,49 +15,6 @@ namespace Lab1.Algorithms
             return tmp;
         }
 
-
-        //AG: this thing I got from the internet http://social.msdn.microsoft.com/Forums/en-US/70408584-668d-49a0-b179-fabf101e71e9/solution-of-linear-equations-systems
-        private void computeCoefficents2(double[,] X, double[] Y)
-        {
-            //Use Guassian Elimination.  I've got source code on these forums somwhere.
-            //This function takes an N X N matrix in the X variable and a 1 X N vector in the Y variable.  The coefficients are returned in the Y variable.
-            int I, J, K, K1, N;
-            N = Y.Length;
-            for (K = 0; K < N; K++)
-            {
-                K1 = K + 1;
-                for (I = K; I < N; I++)
-                {
-                    if (X[I, K] != 0)
-                    {
-                        for (J = K1; J < N; J++)
-                        {
-                            X[I, J] /= X[I, K];
-                        }
-                        Y[I] /= X[I, K];
-                    }
-                }
-                for (I = K1; I < N; I++)
-                {
-                    if (X[I, K] != 0)
-                    {
-                        for (J = K1; J < N; J++)
-                        {
-                            X[I, J] -= X[K, J];
-                        }
-                        Y[I] -= Y[K];
-                    }
-                }
-            }
-            for (I = N - 2; I >= 0; I--)
-            {
-                for (J = N - 1; J >= I + 1; J--)
-                {
-                    Y[I] -= X[I, J] * Y[J];
-                }
-            }
-        }
-
         private void computeCoefficents(double[,] X, double[] Y)
         {
             int n = Y.Length, exchangeI;
@@ -95,11 +52,23 @@ namespace Lab1.Algorithms
                    quotient = X[i, k] / X[k, k] * (-1);
                    for (int j = k; j < n; j++ )
                    {
-                       X[k, j] = X[k, j] * quotient + X[i, j];
+                       X[i, j] = X[k, j] * quotient + X[i, j];
                    }
+                   Y[i] = Y[k] * quotient + Y[i];
                 }
 
                 //calculations from gauss method, part 2
+            }
+            Y[n-1] = Y[n-1] / X[n-1, n-1];
+            double sum;
+            for (int k = n-2; k >= 0; k-- )
+            {
+                sum = 0;
+                for (int i = 1; i < n - k; i++) 
+                {
+                    sum = sum + X[k, n - i] * Y[n - i];
+                }
+                Y [k] = (Y[k] - sum) / X[k, k];
             }
         }
     }
