80f33cdafe407a1ddf3fd86f0326d593b4ee42b8
Petya12345 <agslab@gmail.com>
2013-11-29 11:55:52 +0200

    newtons + formulas

--- Lab1/Lab3/Algorithms/NewtonsCombined.cs
+++ Lab1/Lab3/Algorithms/NewtonsCombined.cs
@@ -11,13 +11,13 @@ namespace Lab3.Algorithms
         {
             var bisectA = a;
             var bisectB = b;
-            new Bisection().FindRoot(ref a, ref b, eps1, function);
+            var bisectionresult = new Bisection().FindRoot(ref bisectA, ref bisectB, eps1, function);
             Console.WriteLine("Interval after bisection: [{0};{1}]", a, b);
-            double xPrev = (a + b) / 2;
+            double xPrev = (bisectA + bisectB) / 2;
             Console.Write("{0} ", xPrev);
             double x = 0;
             int i;
-            for (i = 0; i < 10000; i++) //prevent infinite loop 
+            for (i = 0; i < 100; i++) //prevent infinite loop 
             {
                 x = xPrev - function(xPrev) / derivative(xPrev);
                 Console.Write("{0} ", x);
--- Lab1/Lab3/Program.cs
+++ Lab1/Lab3/Program.cs
@@ -10,13 +10,18 @@ namespace Lab3
         static void Main(string[] args)
         {
 
-            var bisection = new Algorithms.Bisection();
-            double xl = 0, xu = 20;
+            Func<double, double> fn1 = x => x * Math.Pow(Math.E, 0.8 - x); //Andris
+            Func<double, double> fn2 = x => x * x * Math.Sin(x); //Alina
+            Func<double, double> fn3 = x => Math.Pow(Math.Log(x), 2) / x; //Moritz
 
-            double root = bisection.FindRoot(xl, xu, 0.1, x=>Math.Sin(x));
+            Func<double, double> fn1d = x => -2.22554 * Math.Pow(Math.E, (-x)) * (-1 + x); //Andris
+            Func<double, double> fn2d = x => x * (x * Math.Cos(x)+2*Math.Sin(x)); //Alina
+            Func<double, double> fn3d = x => -((-2 + Math.Log(x)) * Math.Log(x)) / Math.Pow(x, 2); //Moritz
 
-
-            Console.WriteLine("X = {0}", root);
+            var newtons = new Algorithms.NewtonsCombined();
+            newtons.FindRoot(fn1, fn1d, -1, 1, 0.1, 0.01);
+            //newtons.FindRoot(fn2, fn2d, 2, 3, 0.1, 0.01);
+            //newtons.FindRoot(fn2, fn2d, 2, 3, 0.1, 0.01);
             Console.Read();
         }
     }
