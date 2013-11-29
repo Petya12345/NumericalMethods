546a0c368b6e31d1e154b9d56c45baf9f1283eb5
Petya12345 <agslab@gmail.com>
2013-11-29 11:20:46 +0200

    newtonsstub

---
+++ Lab1/Lab3/Algorithms/NewtonsCombined.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3.Algorithms
+{
+    class NewtonsCombined
+    {
+        public double FindRoot(Func<double, double> function, Func<double, double> derivative, double a, double b, double eps1, double eps2)
+        {
+            
+            return 0;
+        }
+    }
+}
