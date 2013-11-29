409fcc1e1bbb9018184299d867875178eebbfbd0
Petya12345 <agslab@gmail.com>
2013-11-29 11:34:03 +0200

    newtons

--- Lab1/Lab3/Algorithms/NewtonsCombined.cs
+++ Lab1/Lab3/Algorithms/NewtonsCombined.cs
@@ -9,8 +9,26 @@ namespace Lab3.Algorithms
     {
         public double FindRoot(Func<double, double> function, Func<double, double> derivative, double a, double b, double eps1, double eps2)
         {
-            var 
-            return 0;
+            var bisectA = a;
+            var bisectB = b;
+            new Bisection().FindRoot(ref a, ref b, eps1, function);
+            Console.WriteLine("Interval after bisection: [{0};{1}]", a, b);
+            double xPrev = (a + b) / 2;
+            Console.Write("{0} ", xPrev);
+            double x = 0;
+            int i;
+            for (i = 0; i < 10000; i++) //prevent infinite loop 
+            {
+                x = xPrev - function(xPrev) / derivative(xPrev);
+                Console.Write("{0} ", x);
+                if (Math.Abs(x - xPrev) < eps2)
+                {
+                    break;
+                }
+                xPrev = x;
+            }
+            Console.WriteLine("Finished in {0} iterations", i);
+            return x;
         }
     }
 }
