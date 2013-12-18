78940611e36711323fa50bc809c8cd5509f9d8b7
Petya12345 <agslab@gmail.com>
2013-12-18 12:57:49 +0200

    fix lab4

--- Lab1/Lab3/Algorithms/Bisection.cs
+++ Lab1/Lab3/Algorithms/Bisection.cs
@@ -7,15 +7,16 @@ namespace Lab3.Algorithms
 {
     class Bisection
     {
-        public double FindRoot(ref double xl, ref double xu, double e, Func<double, double> function)
+        public double FindRoot(ref double xl, ref double xu, double e, Func<double, double> function, ref int numOfIterations)
         {
+            numOfIterations++;
             double xr = (xl + xu) / 2;
 
             if (function(xl) * function(xr) < 0) xu = xr;
             else xl = xr;
 
             if ((xu - xl) <= e) return xr;
-            else return FindRoot(ref xl, ref xu, e, function);
+            else return FindRoot(ref xl, ref xu, e, function, ref numOfIterations);
         }
 
         public double f(double x)
--- Lab1/Lab3/Algorithms/NewtonsCombined.cs
+++ Lab1/Lab3/Algorithms/NewtonsCombined.cs
@@ -7,11 +7,11 @@ namespace Lab3.Algorithms
 {
     class NewtonsCombined
     {
-        public double FindRoot(Func<double, double> function, Func<double, double> derivative, double a, double b, double eps1, double eps2)
+        public double FindRoot(Func<double, double> function, Func<double, double> derivative, double a, double b, double eps1, double eps2, ref int numOfIterations)
         {
             var bisectA = a;
             var bisectB = b;
-            var bisectionresult = new Bisection().FindRoot(ref bisectA, ref bisectB, eps1, function);
+            var bisectionresult = new Bisection().FindRoot(ref bisectA, ref bisectB, eps1, function, ref numOfIterations);
             Console.WriteLine("Interval after bisection: [{0};{1}]", bisectA, bisectB);
             double xPrev = (bisectA + bisectB) / 2;
             Console.Write("{0} ", xPrev);
@@ -27,6 +27,7 @@ namespace Lab3.Algorithms
                 }
                 xPrev = x;
             }
+            numOfIterations += i + 1;
             Console.WriteLine("Finished in {0} iterations", i + 1);
             return x;
         }
